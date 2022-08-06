3266059d60ce4cc868d8c5c48409a2241c48c785
mrosyidridho06 <61150937+mrosyidridho06@users.noreply.github.com>
2022-08-06 13:43:20 +0800

    update dashboard admin

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,10 +19,21 @@ class DashboardController extends Controller
         // dd($course);
         $mycor = User::where('role', 'user')->count();
         $murid = User::where('role', 'murid')->count();
+        $admin = User::where('role', 'admin')->count();
 
         $cour = Course::with('mycourse')->get();
 
+        $datarate = MyCourse::with('user', 'course', 'course.lesson')->get();
 
-        return view('admin.dashboard', compact('course', 'mycor', 'murid', 'cour'));
+        // $dataprog = User::all()->lessonstudent()->where('course_id', $item->course->id)->count();
+        // $materi = MyCourse::with('course','user', 'course.lesson')->where([['user_id', Auth::user()->id], ['course_id', $idcor]])->get();
+        // foreach($materi as $itemmat){
+        //     $dl = $itemmat->course->lesson->count();
+        // }
+        // $persen = $dataprog/$dl*100;
+
+
+
+        return view('admin.dashboard', compact('course', 'mycor', 'murid', 'cour', 'datarate', 'admin'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -43,7 +43,24 @@
                     <p>User</p>
                 </div>
                 <div class="icon">
-                    <i class="fas fa-chart-pie"></i>
+                    <i class="fa fa-users"></i>
+                </div>
+                <a href="{{ route('users.index') }}" class="small-box-footer">
+                    More info <i class="fas fa-arrow-circle-right"></i>
+                </a>
+            </div>
+        </div>
+
+        <div class="col-lg-3 col-6">
+            <!-- small card -->
+            <div class="small-box bg-secondary">
+                <div class="inner">
+                    <h3>{{ $admin }}</h3>
+
+                    <p>Admin</p>
+                </div>
+                <div class="icon">
+                    <i class="fas fa-user"></i>
                 </div>
                 <a href="{{ route('users.index') }}" class="small-box-footer">
                     More info <i class="fas fa-arrow-circle-right"></i>
@@ -74,7 +91,43 @@
                                     </td>
                                     <td>{{ $itemcor->mycourse->count() }}</td>
                                 </tr>
-                                @endforeach
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div><!-- /.card -->
+        </div>
+
+        <div class="col-lg-6">
+            <div class="card">
+                <div class="card-header border-0">
+                    <div class="d-flex justify-content-between">
+                        <h3 class="card-title">Complete Rate User in Course</h3>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <table class="table table-hover" id="myTablecourse">
+                        <thead>
+                            <th>Name</th>
+                            <th>Course</th>
+                            <th>Complete Rate</th>
+                        </thead>
+                        <tbody>
+                            @foreach ($datarate as $itemrate)
+                                <tr>
+                                    <td>{{ $itemrate->user->name }}</td>
+                                    <td>
+                                        {{ $itemrate->course->title }}
+                                    </td>
+                                    {{-- <td>{{ round($itemrate->user->lessonstudent()->where('course_id', $itemrate->course->id)->count()/$itemrate->course->lesson->count()*100) }}%</td> --}}
+                                    <td>
+                                        <div class="progress">
+                                            <div class="progress-bar progress-bar-striped bg-info" role="progressbar" style="width: {{ round($itemrate->user->lessonstudent()->where('course_id', $itemrate->course->id)->count()/$itemrate->course->lesson->count()*100) }}%;" aria-valuenow="{{ round($itemrate->user->lessonstudent()->where('course_id', $itemrate->course->id)->count()/$itemrate->course->lesson->count()*100) }}" aria-valuemin="0" aria-valuemax="100">{{ round($itemrate->user->lessonstudent()->where('course_id', $itemrate->course->id)->count()/$itemrate->course->lesson->count()*100) }}%</div>
+                                        </div>
+                                        {{ $itemrate->user->lessonstudent()->count() }} of {{ $itemrate->course->lesson->count() }}
+                                    </td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -86,6 +139,11 @@
     <script>
         $(document).ready( function () {
             $('#myTable').DataTable();
+        } );
+    </script>
+    <script>
+        $(document).ready( function () {
+            $('#myTablecourse').DataTable();
         } );
     </script>
 @endpush
